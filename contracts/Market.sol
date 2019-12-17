8f1bf030d5dd126e8a81d5f21f075f40c10beb07
Kirill <kosten.krll@gmail.com>
2019-12-17 14:59:08 +0200

    Code style fixes

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -4,25 +4,33 @@ import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 
 
 contract Market{
-    
     using SafeMath for uint256;
-
     mapping(string => Proj) public projectsMarket;
 
-
-    function createProject(string memory _name,string memory _description,uint8 _decimals, uint _price)public{
+    function createProject(
+        string memory _name,
+        string memory _description,
+        uint8 _decimals,
+        uint _price
+    )
+    public
+    {
         Proj project = new Proj(_name, _description, _decimals, _price);
         projectsMarket[_name] = project;
     }
-    
+
     function getProjectInfo(string memory _name)
-    public view returns(
-        string memory,
-        string memory,
-        string memory,
-        uint256,
-        string memory,
-        uint256){
+        public
+        view
+        returns(
+            string memory,
+            string memory,
+            string memory,
+            uint256,
+            string memory,
+            uint256
+        )
+    {
         return (
             projectsMarket[_name].name(),
             projectsMarket[_name].symbol(),
@@ -32,19 +40,19 @@ contract Market{
             getAmountOfShares(_name)
             );
     }
-    
-    function getAmountOfShares(string memory _name)public view returns(uint256){
+
+    function getAmountOfShares(string memory _name) public view returns(uint256){
         return projectsMarket[_name].decimals();
     }
-    
-    function getProjectPrice(string memory _name)public view returns(uint256){
+
+    function getProjectPrice(string memory _name) public view returns(uint256){
         return projectsMarket[_name].price();
     }
-    
-    function getBalance(string memory _name)public view returns(uint256){
+
+    function getBalance(string memory _name) public view returns(uint256){
         return projectsMarket[_name].totalSupply();
     }
-    
+
     function sellTokens(string memory _name, address payable buyer,uint256 amount)public payable{
         require(amount>0,'Amount should be greater than 0');
         uint256 price = getProjectPrice(_name);
@@ -57,6 +65,5 @@ contract Market{
         uint256 price = getProjectPrice(_name);
         projectsMarket[_name].transferFrom(seller, tx.origin, amount);
         seller.send(price*amount);
-     
     }
 }
